cb04aa3c4fa35de41e9c5bf4b1c3d94b8b97d723
James Jackson-South <james_south@hotmail.com>
2020-10-15 16:42:46 +0100

    Add reproduction

---
+++ SetupDotNetActionFailRepro/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace SetupDotNetActionFailRepro
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
